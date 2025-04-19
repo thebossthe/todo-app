249def92ad9a9753b1b757d905fc0e924308f011
thebossthe <yamashiro1582@gmai.com>
2025-04-19 17:38:29 +0900

    環境設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [],
@@ -8,3 +9,14 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'your_password',
+  database: 'your_db_name',
+  autoLoadEntities: true,
+  synchronize: true, // 本番ではfalseに！
+})
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
 }
 bootstrap();
+
+fetch('http://localhost:3000/api/hoge')
+  .then(res => res.json())
+  .then(data => console.log(data));
